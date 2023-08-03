d624f244c3fa36e7c6d56efa6d0164fa4e5e94c8
Markosgt7 <markos.guate7@gmail.com>
2023-08-03 12:32:45 -0600

    incio de aplicacion y creacion de modulos necesarios de formulario y listado de personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaPersonaComponent } from './components/lista-persona/lista-persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListaPersonaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lista-persona/lista-persona.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-persona',
+  templateUrl: './lista-persona.component.html',
+  styleUrls: ['./lista-persona.component.css']
+})
+export class ListaPersonaComponent {
+
+}
